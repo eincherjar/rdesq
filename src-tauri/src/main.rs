a1c1c290684f111538f155b50070abbf25c3e6d8
eincherjar <marcinbarszcz2@gmail.com>
2026-07-11 15:41:19 +0200

    v1.2.0 - build Linux on Fedora 41 container in CI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    #[cfg(target_os = "linux")]
-    {
-        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
-        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
-        std::env::set_var("WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS", "1");
-        std::env::set_var("EGL_PLATFORM", "surfaceless");
-    }
     rdesq::run()
 }
